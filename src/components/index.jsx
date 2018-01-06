52a1e476a816791e1bb1d1fc7b388cd5a3f497da
Ringo Yip <ringoyip0901@gmail.com>
2018-01-05 16:51:10 -0800

    boilerplate

---
+++ src/components/index.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            phrase: "ciao"
+        }
+    }
+
+    render() {
+        return (
+            <div>Hello World!!!! and {this.state.phrase}</div>
+        );
+    }
+}
+
+render(
+    <App />,
+    document.getElementById('app')
+)
---
+++ src/devtools/devtools.js
@@ -0,0 +1,6 @@
+chrome.devtools.panels.create(
+    'React-Scope',//title of the panel
+    null, //the path to the icon
+    'index.html', //html page for injecting into the tab's content
+    null //callback function optional
+);
